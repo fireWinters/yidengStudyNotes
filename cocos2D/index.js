a3185923da46920e053bc0cb95b5f19ea8d587a1
唐王瑶 <tangwangyao@hualala.com>
2022-07-04 10:15:52 +0800

    feat: add new module

---
+++ cocos2D/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: callus
+ * @Email：1987567948@qq.com
+ * @Date: 2022-06-30 21:22:28
+ * @Description: 页面/组件/功能的描述
+ * @FilePath: /yidengStudyNotes/cocos2D/index.js
+ */
+// 1下载cocos2d-x-4.x.x.zip
+// 2解压
+// 3安装cocos2d-x-4.x.x.zip
+// 4进入cocos2d-x-4.x.x目录
+// 5运行cocos2d-x-4.x.x/setup.py  (mac自带python，windows需要安装python)
+// 6安装JDK
+// 7安装SDK
+// 8安装NDK
+// 9初始化一个项目
+// 10进入项目目录
